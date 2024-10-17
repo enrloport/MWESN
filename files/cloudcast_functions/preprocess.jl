66bc9e7261fef88e3f6542e1897537ffc85cface
enrloport <e.l.o.universidad@gmail.com>
2024-10-17 09:25:08 +0200

    updates in preprocess: 4 classes

--- files/cloudcast_functions/preprocess.jl
+++ files/cloudcast_functions/preprocess.jl
@@ -5,10 +5,15 @@ Some minor pre-processing is needed to match the cloud types described in CloudC
 1. Remove atipical values (> 253)
 2. Give to classes 1, 2, 3 and 4 (which are cloud-free land, cloud-free sea, snow over land and sea ice. I.e., classes that are not actual cloud types) a value of 0.
 3. Subtract 4 to match the integer values from cloudcast paper and set NaNs = 0, i.e.,
+4. Make 4 superclases: No-cloud, low clouds, mid clouds, high clouds.
 ===#
 
 function cc_to_int(img)
-    return map(x-> x > 253 ? Int8(0) : x-4 > 0 ? Int8((x-4)) : Int8(0) , img)
+    aux = map(x-> x > 253 ? Int8(0) : x-4 > 0 ? Int8((x-4)) : Int8(0) , img)
+    classes = Dict(
+        0 => 0, 1 => 1, 2 => 1, 3 => 2, 4 => 3, 5 => 3, 6 => 1, 7 => 3, 8 => 3, 9 => 3, 10 => 3 
+    )
+    return [classes[x] for x in aux]
 end
 
 function cc_to_float(img)
