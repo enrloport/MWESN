f883ddf79b0b0269ef43d1ad3abd8a3d373e92e7
enrloport <e.l.o.universidad@gmail.com>
2025-03-06 20:35:32 +0100

    updates in mwesn_mnist

--- runs/mnist/MWESN_mnist.jl
+++ runs/mnist/MWESN_mnist.jl
@@ -13,11 +13,11 @@ test_x = transform_mnist(test_x, new_size)
 
 
 # PARAMS
-repit = 1
+repit = 500
 
 _params = Dict{Symbol,Any}(
-     :gpu               => false
-    ,:wb                => false
+     :gpu               => true
+    ,:wb                => true
     ,:confusion_matrix  => true
     ,:wb_logger_name    => "MWESN_mnist__GPU"
     ,:classes           => [0,1,2,3,4,5,6,7,8,9]
@@ -38,8 +38,8 @@ _params = Dict{Symbol,Any}(
 if _params[:gpu] CUDA.allowscalar(false) end
 if _params[:wb] using Logging, Wandb end
 
-mwesn=[]
-# for _ in 1:repit
+for _ in 1:repit
+
     mwesn=[]
     _params[:layers] = [ [2000 for _ in 1:10]]
     _params[:connections] = Dict(
@@ -49,7 +49,7 @@ mwesn=[]
     _params[:active_inputs] = 1:10
     _params[:active_outputs]= 1:10
 
-    sd = 7522#42#rand(1:10000)
+    sd = rand(1:10000)
     Random.seed!(sd)
 
     _params_esn = Dict{Symbol,Any}(
@@ -59,7 +59,7 @@ mwesn=[]
         ,:Win_dens => [rand(Uniform(0.1,0.5),length(layer) ) for layer in _params[:layers]]
         ,:rho      => [rand(Uniform(1.0,4.0),length(layer) ) for layer in _params[:layers]]
         ,:sigma    => [rand(Uniform(0.5,1.5),length(layer) ) for layer in _params[:layers]]
-        ,:sgmds    => [ [tanh for _ in 1:length(_params[:layers][i])] for i in 1:length(_params[:layers]) ]
+        ,:sgmds    => [ [sigmoid for _ in 1:length(_params[:layers][i])] for i in 1:length(_params[:layers]) ]
     )
 
     par = Dict(
@@ -77,10 +77,7 @@ mwesn=[]
         , "Sigmas"              => _params_esn[:sigma]
         , "W_scalings"          => _params_esn[:W_scaling]
         )
-    if _params[:wb]
-        _params[:lg] = wandb_logger(_params[:wb_logger_name])
-        Wandb.log(_params[:lg], par )
-    end
+    
     display(par)
 
     tm = @elapsed begin
@@ -92,41 +89,23 @@ mwesn=[]
             mwesn.test_function(mwesn,_params)
         end
     end
-    _params[:total_time] = tm
-    _params[:train_time] = tm_train
-    _params[:test_time]  = tm_test
 
-    
-    mwesn.error
-    # full_log(_params,_params_esn,mwesn)
-
-    mwesn.H[:,1]
-    mwesn.Y
-    mwesn.classes_Wouts
-
-    cm = confusion_matrix(string.(_params[:classes]), [x[1] for x in mwesn.Y], mwesn.Y_target .% Int8)
-    n,m = size(cm)
-    cm2 = cm ./ sum(cm,dims=1)
-    heatmap(
-        string.(_params[:classes])
-        , string.(_params[:classes])
-        , cm2
-        , title="Confusion matrix"
-        , xlabel="Predicted"
-        , ylabel="Target"
-        , set_yticklabels=string.(_params[:classes])
-        , labels=string.(_params[:classes])                                                          
-        , fc=cgrad([:white,:dodgerblue4])
+    par["Error"] = mwesn.error
+    par["confusion_matrix"] = Wandb.wandb.plot.confusion_matrix(
+            y_true = mwesn.Y_target[1:_params[:test_length]], preds = [x[1] for x in mwesn.Y], class_names = _params[:classes]
         )
-    annotate!([(j-0.5, i-0.5, text(round(cm2[i,j],digits=3), 10,"Computer Modern",:black)) for i in 1:n for j in 1:m])
+
+    if _params[:wb]
+        _params[:lg] = wandb_logger(_params[:wb_logger_name])
+        Wandb.log(_params[:lg], par )
+    end
 
     if _params[:wb]
         close(_params[:lg])
     end
 
-    printime = _params[:gpu] ? "Time GPU: " * string(tm) :  "Time CPU: " * string(tm) 
-    println("\n\n TP: ",_params[:target_pixel],"\nError: ", mwesn.error, "\n", printime  )
 
-# end
+
+end
 
 # EOF
